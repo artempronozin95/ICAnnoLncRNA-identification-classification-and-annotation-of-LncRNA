ed6d92572a6dedbc1a9093c6a7ea64ce891721dd
Artem Pronozin <56677512+artempronozin95@users.noreply.github.com>
2022-02-08 11:06:14 +0700

    Update take_noncoding.py

--- scripts/take_noncoding.py
+++ scripts/take_noncoding.py
@@ -3,7 +3,7 @@ import numpy as np
 from Bio import SeqIO
 import sys
 
-lcnfinder = pd.read_csv(sys.argv[1], sep=',', header=None)
+lncfinder = pd.read_csv(sys.argv[1], sep=',', header=None)
 record_dict = SeqIO.to_dict(SeqIO.parse(sys.argv[2], "fasta"))
 lncrna = open(sys.argv[3], 'w', encoding='utf-8')
 coding = open('data/output/Coding.fasta', 'w', encoding='utf-8')
